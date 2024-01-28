cf67952860b6d333aa7e25aefab308bc22e8d24e
Ryan <ryan@ryro.io>
2024-01-28 18:04:07 -0500

    Adding movie category

---
+++ src/app/library/category/movie/page.tsx
@@ -0,0 +1,32 @@
+
+import LibraryHeader from "@/components/LibraryHeader";
+import { NoteMetaData } from "@/components/NoteMetaData";
+import NotePreview from "@/components/NotePreview";
+
+import getNoteMetaData from "@/components/getNoteMetaData";
+
+
+
+const MoviesPage = (props: any) => {
+    const noteMetaData = getNoteMetaData();
+    const activeParam = "movie"
+    const notePreviews = noteMetaData.filter((note) => {
+      return note.type.toLowerCase() == activeParam;
+    }) 
+    .sort((a, b) => {
+    return new Date(b.firstConsumed).getTime() - new Date(a.firstConsumed).getTime()
+  }).map((note) => (
+    
+    <NotePreview key={note.slug} {...note} />
+    
+  ))
+
+  return(
+    <div>
+      <LibraryHeader {...{active: activeParam}}/>
+      {notePreviews}
+    </div>
+  )
+}
+
+export default MoviesPage;
