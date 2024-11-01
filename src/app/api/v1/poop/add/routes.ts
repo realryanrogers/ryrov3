2f8374dbadb54c07c62d92866c305a1808e52b81
Ryan <ryan@ryro.io>
2024-11-01 09:11:23 -0400

    Adding poop tracker

---
+++ src/app/api/v1/poop/add/routes.ts
@@ -0,0 +1,51 @@
+
+import { PrismaClient, Prisma } from '@prisma/client'
+
+export async function POST(request: Request){
+    try 
+    {
+        const bod = await request.json()
+        console.log(bod["token"])
+        if(bod["token"] != process.env.API_TOKEN) {
+            return new Response('Not Authorized')
+        }
+
+        var poopDateTime = new Date(Date.now())
+        var poopHardness = 2
+        var poopVolume = 2
+        var poopDifficulty = 2
+
+        if(bod["datetime"] != null){
+            poopDateTime = new Date(bod["datetime"])
+        } 
+        if(bod["hardness"] != null){
+            poopHardness = parseInt(bod["hardness"])
+        }
+        if(bod["volume"] != null){
+            poopVolume = parseInt(bod["volume"])
+        }
+        if(bod["difficulty"] != null){
+            poopDifficulty = parseInt(bod["difficulty"])
+        }
+
+        const prisma = new PrismaClient()
+
+        const poopResult = await prisma.poop.create({
+            data: {
+                hardness: poopHardness,
+                volume: poopVolume,
+                datetime: poopDateTime,
+                difficulty: poopDifficulty
+            }
+        })
+
+        return Response.json(poopResult)
+        
+    } catch (error) {
+        return new Response('Not Ok')
+    }
+    
+
+
+    return Response.json({})
+}
